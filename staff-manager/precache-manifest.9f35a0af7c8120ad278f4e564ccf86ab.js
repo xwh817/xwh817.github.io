231a1aeae77d33081d901bc6358ff9f3ef23b800
xwh817 <xwh817@163.com>
2019-03-21 21:52:54 +0800

    add react build pages

---
+++ staff-manager/precache-manifest.9f35a0af7c8120ad278f4e564ccf86ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac35e5775ac4b1ee664",
+    "url": "/static/css/main.8707360a.chunk.css"
+  },
+  {
+    "revision": "2ac35e5775ac4b1ee664",
+    "url": "/static/js/main.809cb1ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51fe18610bcd857ec5b0",
+    "url": "/static/css/2.7f5e905e.chunk.css"
+  },
+  {
+    "revision": "51fe18610bcd857ec5b0",
+    "url": "/static/js/2.9f7add32.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95b13c57c59bcc7f5e450c17e66a96eb",
+    "url": "/index.html"
+  }
+];
