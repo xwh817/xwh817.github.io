fe0a5980ddb7e329ea09ddba7a9239a9c06dda7a
xwh817 <xwh817@163.com>
2019-03-21 22:04:27 +0800

    add react pages

---
+++ staff-manager/precache-manifest.180eb077c7cfa9ce84518a042987ba8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6df668dc1b696b2ee2d",
+    "url": "./static/css/main.8707360a.chunk.css"
+  },
+  {
+    "revision": "f6df668dc1b696b2ee2d",
+    "url": "./static/js/main.609aa123.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "283aaf71aa12ac543429",
+    "url": "./static/css/2.7f5e905e.chunk.css"
+  },
+  {
+    "revision": "283aaf71aa12ac543429",
+    "url": "./static/js/2.ac508242.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "daac6cdb17f028e4e67737bb62ce2c27",
+    "url": "./index.html"
+  }
+];
--- staff-manager/service-worker.js
+++ staff-manager/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f35a0af7c8120ad278f4e564ccf86ab.js"
+  "./precache-manifest.180eb077c7cfa9ce84518a042987ba8d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
